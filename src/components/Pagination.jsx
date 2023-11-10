0e10ff47809f5f1e86613febd877584b0c18b034
Jesus Molina <netod90@gmail.com>
2023-11-10 13:20:52 -0700

    Add pagination logic

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,30 +4,58 @@ const Pagination = ({
   setCurrentPage,
   totalProducts,
 }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const onPreviousPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+
+  const onNextPage = () => {
+    if (currentPage <= pageNumbers.length - 1) setCurrentPage(currentPage + 1);
+  };
+
+  const onSpecificPage = (noPage) => {
+    setCurrentPage(noPage);
+  };
+
   return (
     <nav
       className="pagination is-centered mb-6"
       role="navigation"
       aria-label="pagination"
     >
-      <a className="pagination-previous">Previous</a>
-      <a className="pagination-next">Next page</a>
+      <a
+        className={`pagination-previous ${
+          currentPage <= pageNumbers.length ? 'is-disabled' : ''
+        }`}
+        onClick={onPreviousPage}
+      >
+        Previous
+      </a>
+      <a
+        className={`pagination-next ${
+          currentPage === pageNumbers.length ? 'is-disabled' : ''
+        }`}
+        onClick={onNextPage}
+      >
+        Next page
+      </a>
       <ul className="pagination-list">
-        <li>
-          <a className="pagination-link" aria-label="Goto page 1">
-            1
-          </a>
-        </li>
-
-        <li>
-          <a
-            className="pagination-link is-current"
-            aria-label="Page 46"
-            aria-current="page"
-          >
-            46
-          </a>
-        </li>
+        {pageNumbers.map((noPage, index) => (
+          <li key={index}>
+            <a
+              className={`pagination-link ${
+                noPage === currentPage ? 'is-current' : ''
+              }`}
+              onClick={() => onSpecificPage(noPage)}
+            >
+              {noPage}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
