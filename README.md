d32e2f116024bd90095d36faf7dc327219c25ac1
Jesus Molina <netod90@gmail.com>
2023-12-20 05:38:02 -0700

    Add disable no-used-vars

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fake-shop
+
+Using bulma.io framework
+API: https://fakestoreapi.com/
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import getProducts from '../services/getProducts';
 import Pagination from './Pagination';
